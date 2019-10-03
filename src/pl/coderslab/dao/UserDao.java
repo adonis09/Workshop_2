8b0eeaa58b438e03db8255b5697ef870913c2cb1
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-03 21:41:08 +0200

    created one-to-many relationship between  and  tables

--- src/pl/coderslab/dao/UserDao.java
+++ src/pl/coderslab/dao/UserDao.java
@@ -9,11 +9,11 @@ import java.util.Arrays;
 public class UserDao {
 
     private static final String CREATE_USER_QUERY =
-            "INSERT INTO user(username, email, password) VALUES (?, ?, ?)";
+            "INSERT INTO user(username, email, password, user_group_id) VALUES (?, ?, ?, ?)";
     private static final String READ_USER_QUERY =
             "SELECT * FROM user where id = ?";
     private static final String UPDATE_USER_QUERY =
-            "UPDATE user SET username = ?, email = ?, password = ? where id = ?";
+            "UPDATE user SET username = ?, email = ?, password = ?, user_group_id = ? where id = ?";
     private static final String DELETE_USER_QUERY =
             "DELETE FROM user WHERE id = ?";
     private static final String FIND_ALL_USERS_QUERY =
@@ -26,6 +26,7 @@ public class UserDao {
             statement.setString(1, user.getUserName());
             statement.setString(2, user.getEmail());
             statement.setString(3, user.getPassword());
+            statement.setInt(4, user.getUserGroupId());
             statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             if (resultSet.next()) {
@@ -49,6 +50,7 @@ public class UserDao {
                 user.setUserName(resultSet.getString("username"));
                 user.setEmail(resultSet.getString("email"));
                 user.setPassword(resultSet.getString("password"));
+                user.setUserGroupId(resultSet.getInt("user_group_id"));
                 return user;
             }
         } catch (SQLException e) {
@@ -63,7 +65,8 @@ public class UserDao {
             statement.setString(1, user.getUserName());
             statement.setString(2, user.getEmail());
             statement.setString(3, user.getPassword());
-            statement.setInt(4, user.getId());
+            statement.setInt(4, user.getUserGroupId());
+            statement.setInt(5, user.getId());
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -97,6 +100,7 @@ public class UserDao {
                 user.setUserName(resultSet.getString("username"));
                 user.setEmail(resultSet.getString("email"));
                 user.setPassword(resultSet.getString("password"));
+                user.setUserGroupId(resultSet.getInt("user_group_id"));
                 users = addToArray(user, users);
             }
             return users;
