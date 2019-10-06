314e3bab22c66ffb43e2ef01ceae5d9588119e9e
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-06 14:51:16 +0200

    corrected the constructor in Group class

--- src/pl/coderslab/model/Group.java
+++ src/pl/coderslab/model/Group.java
@@ -4,8 +4,7 @@ public class Group {
     private int id;
     private String name;
 
-    public Group(int id, String name) {
-        this.id = id;
+    public Group(String name) {
         this.name = name;
     }
 
